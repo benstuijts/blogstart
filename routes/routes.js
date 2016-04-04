d65af69fb57bd19cdc89334cad1d086a17f06f9e
benstuijts <stuijts@spijkenisse.nl>
2016-04-05 01:17:36 +0200

    Update routes.js

--- routes/routes.js
+++ routes/routes.js
@@ -1,1 +1,20 @@
+const express       = require("express");
+const router        = express.Router();
+const bodyParser    = require('body-parser');
+const session       = require('express-session');
+const mongoose      = require('mongoose');
 
+/* Middleware */
+router.use( bodyParser.json() );       // to support JSON-encoded bodies
+router.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+    extended: true
+})); 
+
+router.get('/', function(req, res){
+    res.locals.add({
+        "EXTRA" : "extra local value"
+    }); 
+    res.send(res.locals);
+});
+
+module.exports = router;
