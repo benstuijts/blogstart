5406dbcbd8e929176e6e56fbc83ea50343a0b982
benstuijts <stuijts@spijkenisse.nl>
2016-04-05 01:16:19 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,17 +1,14 @@
 'use strict';
-
-const arg = process.argv.slice(2);
-const app_config;
-process.argv.forEach(function(value){
-  var key = value.split(":")[0],
-      val = value.split(":")[1];
-app_config[key] = val;
+var config = [];
+process.argv.slice(2).forEach(function(value) {
+  var key = value.split(":")[0];
+  var val = value.split(":")[1];
+  config[key] = val;
 });
-
-const http = require("http");
-const express = require("express");
-const app           = express();
-const config  = require("./config/init");
+const http      = require("http");
+const express   = require("express");
+const app       = express();
+const init      = require("./config/init");
 
 app.set('view engine', 'ejs');
 app.set('views', './views');
@@ -19,17 +16,18 @@ app.set('views', './views');
 app.use(express.static('public'));
 
 /* Middleware */
-const config = function(req,res, next) {
-  let c = config[app_config['mode']];
-  res.locals = {
-    c,
+const configuration = function(req,res, next) {
+  res.locals = require("./config/init")[config['mode']];
+  res.locals['add'] = function(obj) {
+    for(var key in obj) {
+      this[key] = obj[key];
+    }
   }
-  console.log(c);
-  console.log(res.locals);
+  next();
 }
 
-app.use('/admin', require('./routes/admin'));
-app.use('/', require('./routes/routes'));
+app.use('/admin', configuration, require('./routes/admin'));
+app.use('/'     , configuration, require('./routes/routes'));
 
 app.listen(8080, function () {
   console.log('Blog listening on port 8080 in ' + config['mode'] + ' mode.');
